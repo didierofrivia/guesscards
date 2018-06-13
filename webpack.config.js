d9f4d7d7d0b3a5b0d3f25ef8372d4b0cd1d13786
dd di cesare <didi@posteo.net>
2018-06-13 10:12:30 +0200

    [webpack] setup with babel and dev server

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs'
+    })
+  ]
+}
