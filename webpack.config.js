021228b13c4eb5511267add24e91b102104f07d9
dd di cesare <didi@posteo.net>
2018-06-14 10:29:18 +0200

    [webpack] passing NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = env => {
+  return {
+    mode: env.NODE_ENV,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
         }
-      }
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.ejs'
+      })
     ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.ejs'
-    })
-  ]
+  }
 }
