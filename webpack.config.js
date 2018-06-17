04c1d2c3a0790a4da78333f3c77ee7689111bde8
dd di cesare <didi@posteo.net>
2018-06-17 19:23:53 +0200

    [webpack] img and fonts support

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,58 @@ module.exports = env => {
               }
             }
           ]
-        }
+        },
+        {
+        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/font-woff'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/octet-stream'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'image/svg+xml'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
       ]
     },
     devServer: {
